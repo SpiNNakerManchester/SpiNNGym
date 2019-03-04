2a47a3da35b0fa1e6e7bebc136a25d4f4aeb82b2
oliverrhodes <oliver.rhodes@manchester.ac.uk>
2019-03-04 13:13:13 +0000

    Added skeleton code for inverted pendulum

--- spinn_gym/__init__.py
+++ spinn_gym/__init__.py
@@ -1,22 +1,9 @@
-# from model_binaries import __file__ as binaries_path
 from games.breakout.breakout import Breakout
 from games.multi_arm_bandit.bandit import Bandit
-# from visualiser.visualiser import Visualiser
-# from visualiser.visualiser_subsamp import Visualiser_subsamp
-
-from spynnaker.pyNN.abstract_spinnaker_common import AbstractSpiNNakerCommon
-
-# import __file__
-# binary_path = os.path.abspath(__file__) +
+from games.inverted_pendulum.inverted_pendulum import InvertedPendulum
 import os
 
+# Put model_binaries directory on path
+from spynnaker.pyNN.abstract_spinnaker_common import AbstractSpiNNakerCommon
 binary_path = os.path.join(os.path.split(__file__)[0], 'model_binaries')
-print binary_path
-# print os.path.join(__file__, os.path.pardir+'/model_binaries')
-
-# AbstractSpiNNakerCommon.register_binary_search_path(os.path.dirname(binaries_path))
 AbstractSpiNNakerCommon.register_binary_search_path(binary_path)
-
-# This adds the model binaries path to the paths searched by sPyNNaker
-# ef = executable_finder.ExecutableFinder(os.path.dirname(binaries_path))
-# executable_finder.add_path(os.path.dirname(binaries_path))
