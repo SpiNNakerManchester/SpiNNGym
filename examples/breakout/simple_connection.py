223eb58f0e4b2813e3b9323b75bc682153f33c8a
Stefan Michael Trett <stefan.trett@thehutgroup.com>
2019-11-17 15:25:01 +0000

    Get Pad and Ball population

--- examples/breakout/simple_connection.py
+++ examples/breakout/simple_connection.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import spynnaker8 as p
 import spinn_gym as gym
-from spynnaker.pyNN.connections.\
+from spynnaker.pyNN.connections. \
     spynnaker_live_spikes_connection import SpynnakerLiveSpikesConnection
 from spinn_front_end_common.utilities.globals_variables import get_simulator
 from spinn_front_end_common.utilities.database.database_connection \
@@ -28,11 +28,11 @@ from spynnaker.pyNN.models.utility_models.spike_injector import \
     SpikeInjector
 from spinn_gym.games.breakout.visualiser.visualiser import Visualiser
 
-
 # -----------------------------------------------------------------------------
 #  Globals
 # -----------------------------------------------------------------------------
-vis_proc = None # Visuliser process (global)
+vis_proc = None  # Visualiser process (global)
+
 
 # -----------------------------------------------------------------------------
 #  Helper Functions
@@ -40,7 +40,7 @@ vis_proc = None # Visuliser process (global)
 def start_visualiser(database, pop_label, xr, yr, xb=8, yb=8, key_conn=None):
     _, _, _, board_address, tag = database.get_live_output_details(
         pop_label, "LiveSpikeReceiver")
-    
+
     print("Calling \'start_visualiser\'")
 
     # Calling visualiser - must be done as process rather than on thread due to
@@ -56,7 +56,8 @@ def start_visualiser(database, pop_label, xr, yr, xb=8, yb=8, key_conn=None):
          ])
     # print("Visualiser proc ID: {}".format(vis_proc.pid))
 
-def get_scores(breakout_pop,simulator):
+
+def get_scores(breakout_pop, simulator):
     b_vertex = breakout_pop._vertex
     scores = b_vertex.get_data(
         'score', simulator.no_machine_time_steps, simulator.placements,
@@ -64,6 +65,7 @@ def get_scores(breakout_pop,simulator):
         simulator.machine_time_step)
 
     return scores.tolist()
+
 
 def row_col_to_input_breakout(row, col, is_on_input, row_bits, event_bits=1,
                               colour_bits=2, row_start=0):
@@ -85,7 +87,6 @@ def row_col_to_input_breakout(row, col, is_on_input, row_bits, event_bits=1,
 
 def subsample_connection(x_res, y_res, subsamp_factor_x, subsamp_factor_y,
                          weight, coord_map_func):
-
     # subY_BITS=int(np.ceil(np.log2(y_res/subsamp_factor)))
     connection_list_on = []
     connection_list_off = []
@@ -104,12 +105,26 @@ def subsample_connection(x_res, y_res, subsamp_factor_x, subsamp_factor_y,
 
             # OFF channels only on segment borders
             # if((j+1)%(y_res/subsamp_factor)==0 or
-            #(i+1)%(x_res/subsamp_factor)==0 or j==0 or i==0):
+            # (i+1)%(x_res/subsamp_factor)==0 or j==0 or i==0):
             connection_list_off.append((coord_map_func(j, i, 0, row_bits),
                                         subsampidx, weight, 1.))
 
     return connection_list_on, connection_list_off
 
+
+def separate_connections(ball_population_size, connections_on):
+    pad_list = []
+    ball_list = []
+
+    for idx, val in enumerate(connections_on):
+        if idx < ball_population_size:
+            ball_list.append(val)
+        else:
+            index_in_pad_pop = idx - ball_population_size
+            new_el_mapping = (val[0], index_in_pad_pop, val[2], val[3])
+            pad_list.append(new_el_mapping)
+
+    return ball_list, pad_list
 
 
 # -----------------------------------------------------------------------------
@@ -133,7 +148,6 @@ x_factor1 = 2
 y_factor1 = x_factor1
 bricking = 1
 
-
 # -----------------------------------------------------------------------------
 # Create Spiking Neural Network
 # -----------------------------------------------------------------------------
@@ -155,34 +169,53 @@ p.Projection(key_input, breakout_pop, p.AllToAllConnector(),
 # Create random spike input and connect to Breakout pop to stimulate paddle
 # (and enable paddle visualisation)
 spike_input = p.Population(2, p.SpikeSourcePoisson(rate=2),
-                            label="input_connect")
+                           label="input_connect")
 p.Projection(spike_input, breakout_pop, p.AllToAllConnector(),
              p.StaticSynapse(weight=0.1))
 
 weight = 0.1
 [Connections_on, Connections_off] = subsample_connection(
-    X_RESOLUTION/x_factor1, Y_RESOLUTION/y_factor1, 1, 1, weight,
+    X_RESOLUTION / x_factor1, Y_RESOLUTION / y_factor1, 1, 1, weight,
     row_col_to_input_breakout)
 
-# Create population of neurons to receive input from Breakout
-receive_pop_size = int(X_RESOLUTION/x_factor1) * int(Y_RESOLUTION/y_factor1)
-receive_pop = p.Population(receive_pop_size, p.IF_cond_exp(),
-                             label="receive_pop")
-p.Projection(breakout_pop, receive_pop, p.FromListConnector(Connections_on),
+# Population sizes
+total_receive_pop_size = int(X_RESOLUTION / x_factor1) * int(Y_RESOLUTION / y_factor1)
+
+pad_pop_size = int(X_RESOLUTION / x_factor1)
+ball_pop_size = total_receive_pop_size - pad_pop_size
+
+[Ball_connections, Pad_connections] = separate_connections(ball_pop_size, Connections_on)
+
+# # Create population of neurons to receive input from Breakout
+# receive_pop = p.Population(total_receive_pop_size, p.IF_cond_exp(),
+#                            label="receive_pop")
+# p.Projection(breakout_pop, receive_pop, p.FromListConnector(Connections_on),
+#              p.StaticSynapse(weight=weight))
+
+# Create the Pad position population
+pad_pop = p.Population(pad_pop_size, p.IF_cond_exp(),
+                       label="pad_pop")
+p.Projection(breakout_pop, pad_pop, p.FromListConnector(Pad_connections),
              p.StaticSynapse(weight=weight))
 
-# Create population to receive reward signal from Breakout (n0: rew, n1: pun)
+# Create the Ball trajectory population
+ball_pop = p.Population(ball_pop_size, p.IF_cond_exp(),
+                        label="ball_pop")
+p.Projection(breakout_pop, ball_pop, p.FromListConnector(Ball_connections),
+             p.StaticSynapse(weight=weight))
+
+# Create population to receive reward signal from Breakout (n0: reward, n1: punishment)
 receive_reward_pop = p.Population(2, p.IF_cond_exp(),
-                             label="receive_rew_pop")
-p.Projection(breakout_pop, receive_reward_pop, p.OneToOneConnector(), 
+                                  label="receive_rew_pop")
+p.Projection(breakout_pop, receive_reward_pop, p.OneToOneConnector(),
              p.StaticSynapse(weight=0.1 * weight))
-       
 
 # Setup recording
 spike_input.record('spikes')
-receive_pop.record('spikes')
+pad_pop.record('spikes')
+ball_pop.record('spikes')
+# receive_pop.record('spikes')
 receive_reward_pop.record('all')
-
 
 # -----------------------------------------------------------------------------
 # Configure Visualiser
@@ -190,27 +223,26 @@ receive_reward_pop.record('all')
 print("UDP_PORT1: {}".format(UDP_PORT1))
 print("x_fact: {}, y_fact: {}".format(x_factor1, y_factor1))
 print("x_bits: {}, y_bits: {}".format(
-    np.uint32(np.ceil(np.log2(X_RESOLUTION/x_factor1))),
-    np.uint32(np.ceil(np.log2(Y_RESOLUTION/y_factor1)))
-    ))
+    np.uint32(np.ceil(np.log2(X_RESOLUTION / x_factor1))),
+    np.uint32(np.ceil(np.log2(Y_RESOLUTION / y_factor1)))
+))
 
 d_conn = DatabaseConnection(local_port=None)
 
 print("\nRegister visualiser process")
 d_conn.add_database_callback(functools.partial(
     start_visualiser, pop_label=b1.label, xr=x_factor1, yr=y_factor1,
-    xb=np.uint32(np.ceil(np.log2(X_RESOLUTION/x_factor1))),
-    yb=np.uint32(np.ceil(np.log2(Y_RESOLUTION/y_factor1))),
+    xb=np.uint32(np.ceil(np.log2(X_RESOLUTION / x_factor1))),
+    yb=np.uint32(np.ceil(np.log2(Y_RESOLUTION / y_factor1))),
     key_conn=key_input_connection))
 
 p.external_devices.add_database_socket_address(
-     "localhost", d_conn.local_port, None)
-
+    "localhost", d_conn.local_port, None)
 
 # -----------------------------------------------------------------------------
 # Run Simulation
 # -----------------------------------------------------------------------------
-runtime = 1000 * 60
+runtime = 1000 * 30
 simulator = get_simulator()
 print("\nLet\'s play breakout!")
 p.run(runtime)
@@ -221,20 +253,25 @@ p.run(runtime)
 print("\nSimulation Complete - Extracting Data and Post-Processing")
 
 spike_input_spikes = spike_input.get_data('spikes')
-receive_pop_spikes = receive_pop.get_data('spikes')
+pad_pop_spikes = pad_pop.get_data('spikes')
+ball_pop_spikes = ball_pop.get_data('spikes')
+# receive_pop_spikes = receive_pop.get_data('spikes')
 receive_reward_pop_output = receive_reward_pop.get_data()
 
-figure_filename = "results.png"
 Figure(
     # raster plot of the presynaptic neuron spike times
     Panel(spike_input_spikes.segments[0].spiketrains,
           yticks=True, markersize=0.2, xlim=(0, runtime)),
-    Panel(receive_pop_spikes.segments[0].spiketrains,
+
+    Panel(pad_pop_spikes.segments[0].spiketrains,
           yticks=True, markersize=0.2, xlim=(0, runtime)),
+    Panel(ball_pop_spikes.segments[0].spiketrains,
+          yticks=True, markersize=0.2, xlim=(0, runtime)),
+
     Panel(receive_reward_pop_output.segments[0].filter(name='gsyn_exc')[0],
-          ylabel="gsyn excitatory (mV)", 
-          data_labels=[receive_reward_pop.label], 
-          yticks=True, 
+          ylabel="gsyn excitatory (mV)",
+          data_labels=[receive_reward_pop.label],
+          yticks=True,
           xlim=(0, runtime)
           )
     # title="Simple Breakout Example"
@@ -245,10 +282,7 @@ plt.show()
 scores = get_scores(breakout_pop=breakout_pop, simulator=simulator)
 print("Scores: {}".format(scores))
 
-
 # End simulation
 p.end()
 vis_proc.terminate()
 print("Simulation Complete")
-
-
