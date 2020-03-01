03d998f5689afff9c9937f7a0c66277629f8571f
Stefan Michael Trett <stefantrett@gmail.com>
2020-03-01 16:44:31 +0000

    Update plot_weights.py

--- examples/breakout/plot_weights.py
+++ examples/breakout/plot_weights.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 FILENAME = "connections.json"
+hidden_pop_size = 150
+X_RES = 80
+Y_RES = 64
 
 
 def prepare_conn_for_plotting(connections, dimensions):
@@ -33,31 +36,31 @@ right_decision_conn = previous_connections[7]
 fig, axs = plt.subplots(3)
 fig.suptitle("Weights to Left Hidden")
 axs[0].set_title("Ball X")
-axs[0].matshow(prepare_conn_for_plotting(ball_x_left_conn, (80, 500)))
+axs[0].matshow(prepare_conn_for_plotting(ball_x_left_conn, (X_RES, hidden_pop_size)))
 axs[1].set_title("Ball Y")
-axs[1].matshow(prepare_conn_for_plotting(ball_y_left_conn, (64, 500)))
+axs[1].matshow(prepare_conn_for_plotting(ball_y_left_conn, (Y_RES, hidden_pop_size)))
 axs[2].set_title("Paddle X")
-axs[2].matshow(prepare_conn_for_plotting(paddle_left_conn, (80, 500)))
+axs[2].matshow(prepare_conn_for_plotting(paddle_left_conn, (X_RES, hidden_pop_size)))
 
 plt.show()
 
 fig, axs = plt.subplots(3)
 fig.suptitle("Weights to Right Hidden")
 axs[0].set_title("Ball X")
-axs[0].matshow(prepare_conn_for_plotting(ball_x_right_conn, (80, 500)))
+axs[0].matshow(prepare_conn_for_plotting(ball_x_right_conn, (X_RES, hidden_pop_size)))
 axs[1].set_title("Ball Y")
-axs[1].matshow(prepare_conn_for_plotting(ball_y_right_conn, (64, 500)))
+axs[1].matshow(prepare_conn_for_plotting(ball_y_right_conn, (Y_RES, hidden_pop_size)))
 axs[2].set_title("Paddle X")
-axs[2].matshow(prepare_conn_for_plotting(paddle_right_conn, (80, 500)))
+axs[2].matshow(prepare_conn_for_plotting(paddle_right_conn, (X_RES, hidden_pop_size)))
 
 plt.show()
 
 fig, axs = plt.subplots(2)
 fig.suptitle("Weights to Decision Hidden")
 axs[0].set_title("Left Hidden")
-axs[0].plot(prepare_conn_for_plotting(left_decision_conn, (500, 2)), 'o')
+axs[0].plot(prepare_conn_for_plotting(left_decision_conn, (hidden_pop_size, 2)), 'o')
 axs[1].set_title("Right Hidden")
-axs[1].plot(prepare_conn_for_plotting(right_decision_conn, (500, 2)), 'o')
+axs[1].plot(prepare_conn_for_plotting(right_decision_conn, (hidden_pop_size, 2)), 'o')
 
 plt.show()
 
