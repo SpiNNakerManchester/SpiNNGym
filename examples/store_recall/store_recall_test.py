fdaac3c885f6570b8348b45fda5c7b928010db19
such-a-git <adamperrett3142@gmail.com>
2020-10-06 13:28:40 +0100

    Change to score print out and accuracy calculation

--- examples/store_recall/store_recall_test.py
+++ examples/store_recall/store_recall_test.py
@@ -64,12 +64,12 @@ scores = get_scores(recall_pop=recall_pop, simulator=simulator)
 print(scores)
 
 i = 0
-print("score  \t\t|\t\t  trials")
+print("score 0 \t\t|\t score 1 \t|\t\t  trials")
 while i < len(scores):
-    print("{:8}\t{:8}".format(scores[i][0], scores[i+1][0]))
-    i += 2
+    print("{:8}\t\t{:8}\t\t{:8}".format(scores[i][0], scores[i+1][0], scores[i+2][0]))
+    i += 3
 
-accuracy = float(scores[len(scores)-2][0]) / float(scores[len(scores)-1][0])
+accuracy = float(scores[len(scores)-2][0]+scores[len(scores)-3][0]) / float(scores[len(scores)-1][0])
 print("Accuracy:", accuracy)
 
 spikes_in = input_pop.get_data('spikes').segments[0].spiketrains
