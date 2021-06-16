a428bafc2f90d25058caddc19c89a4e827d69b67
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2021-06-16 15:55:06 +0100

    Take out the math

--- spinn_gym/games/breakout/breakout.py
+++ spinn_gym/games/breakout/breakout.py
@@ -14,7 +14,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import print_function
-import math
 import numpy
 
 from spinn_utilities.overrides import overrides
