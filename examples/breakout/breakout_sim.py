eb6d21a8802b58794717fd0f94bae3225bfaa70c
test <test@test.com>
2021-10-05 14:22:20 +0200

    Add missing files

---
+++ examples/breakout/breakout_sim.py
@@ -0,0 +1,66 @@
+# Copyright (c) 2019-2021 The University of Manchester
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+def get_scores(breakout_pop, simulator):
+    b_vertex = breakout_pop._vertex
+    scores = b_vertex.get_data(
+        'score', simulator.no_machine_time_steps, simulator.placements,
+        simulator.buffer_manager)
+
+    return scores.tolist()
+
+
+def row_col_to_input_breakout(row, col, is_on_input, row_bits, event_bits=1,
+                              colour_bits=2, row_start=0):
+    row_bits = np.uint32(row_bits)
+    idx = np.uint32(0)
+
+    if is_on_input:
+        idx = 1
+
+    row += row_start
+    idx = idx | (row << (colour_bits))  # colour bit
+    idx = idx | (col << (row_bits + colour_bits))
+
+    # add two to allow for special event bits
+    idx = idx + 2
+
+    return idx
+
+
+def subsample_connection(x_res, y_res, subsamp_factor_x, subsamp_factor_y,
+                         weight, coord_map_func):
+    # subY_BITS=int(np.ceil(np.log2(y_res/subsamp_factor)))
+    connection_list_on = []
+    connection_list_off = []
+
+    sx_res = int(x_res) // int(subsamp_factor_x)
+    row_bits = int(np.ceil(np.log2(y_res)))
+    for j in range(int(y_res)):
+        for i in range(int(x_res)):
+            si = i // subsamp_factor_x
+            sj = j // subsamp_factor_y
+
+            # ON channels
+            subsampidx = sj * sx_res + si
+            connection_list_on.append((coord_map_func(j, i, 1, row_bits),
+                                       subsampidx, weight, 1.))
+
+            # OFF channels only on segment borders
+            connection_list_off.append((coord_map_func(j, i, 0, row_bits),
+                                        subsampidx, weight, 1.))
+
+    return connection_list_on, connection_list_off
