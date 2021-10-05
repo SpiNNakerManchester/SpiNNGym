2f7fc708c5936d9559d60281d569c8f1c9f7432d
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2021-10-05 14:50:46 +0100

    Started moving things

--- examples/breakout/breakout_sim.py
+++ examples/breakout/breakout_sim.py
@@ -12,6 +12,9 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import numpy
+from spinn_gym.games.breakout.breakout import Breakout
+import spynnaker8 as p
 
 
 def get_scores(breakout_pop, simulator):
@@ -25,8 +28,8 @@ def get_scores(breakout_pop, simulator):
 
 def row_col_to_input_breakout(row, col, is_on_input, row_bits, event_bits=1,
                               colour_bits=2, row_start=0):
-    row_bits = np.uint32(row_bits)
-    idx = np.uint32(0)
+    row_bits = numpy.uint32(row_bits)
+    idx = numpy.uint32(0)
 
     if is_on_input:
         idx = 1
@@ -48,7 +51,7 @@ def subsample_connection(x_res, y_res, subsamp_factor_x, subsamp_factor_y,
     connection_list_off = []
 
     sx_res = int(x_res) // int(subsamp_factor_x)
-    row_bits = int(np.ceil(np.log2(y_res)))
+    row_bits = int(numpy.ceil(numpy.log2(y_res)))
     for j in range(int(y_res)):
         for i in range(int(x_res)):
             si = i // subsamp_factor_x
@@ -64,3 +67,64 @@ def subsample_connection(x_res, y_res, subsamp_factor_x, subsamp_factor_y,
                                         subsampidx, weight, 1.))
 
     return connection_list_on, connection_list_off
+
+
+def make_simulation(
+        x_res=160, y_res=128, x_scale=2, y_scale=2, breakout_label="Breakout",
+        key_input_label="key_input", send_key_poisson=True):
+
+    # Setup pyNN simulation
+    p.setup(timestep=1.0)
+    p.set_number_of_neurons_per_core(p.IF_cond_exp, 128)
+
+    # -----------------------------------------------------------------------------
+    # Create Spiking Neural Network
+    # -----------------------------------------------------------------------------
+
+    # Create breakout population and activate live output
+    b1 = Breakout(x_factor=x_scale, y_factor=y_scale, bricking=1)
+    breakout_pop = p.Population(b1.neurons(), b1, label=breakout_label)
+
+    # Live output the breakout population
+    p.external_devices.activate_live_output_for(breakout_pop)
+
+    # Connect key spike injector to breakout population
+    key_input = p.Population(
+        2, p.external_devices.SpikeInjector, label=key_input_label)
+    p.Projection(
+        key_input, breakout_pop, p.AllToAllConnector(),
+        p.StaticSynapse(weight=0.1))
+
+    # Create random spike input and connect to Breakout pop to stimulate paddle
+    # (and enable paddle visualisation)
+    spike_input = p.Population(2, p.SpikeSourcePoisson(rate=2),
+                               label="input_connect")
+    p.Projection(
+        spike_input, breakout_pop, p.AllToAllConnector(),
+        p.StaticSynapse(weight=0.1))
+
+    weight = 0.1
+    [Connections_on, _] = subsample_connection(
+        x_res / x_scale, y_res / y_scale, 1, 1, weight,
+        row_col_to_input_breakout)
+
+    # Create population of neurons to receive input from Breakout
+    receive_pop_size = int(x_res / x_scale) * int(y_res / y_scale)
+    receive_pop = p.Population(receive_pop_size, p.IF_cond_exp(),
+                               label="receive_pop")
+    p.Projection(
+        breakout_pop, receive_pop, p.FromListConnector(Connections_on),
+        p.StaticSynapse(weight=weight))
+
+    # Create population to receive reward signal from Breakout
+    # (n0: rew, n1: pun)
+    receive_reward_pop = p.Population(
+        2, p.IF_cond_exp(), label="receive_rew_pop")
+    p.Projection(
+        breakout_pop, receive_reward_pop, p.OneToOneConnector(),
+        p.StaticSynapse(weight=0.1 * weight))
+
+    # Setup recording
+    spike_input.record('spikes')
+    receive_pop.record('spikes')
+    receive_reward_pop.record('all')
--- examples/breakout/breakout_simple_connection.py
+++ examples/breakout/breakout_simple_connection.py
@@ -21,6 +21,8 @@ import functools
 import subprocess
 import sys
 
+from examples.breakout.breakout_sim import make_simulation
+
 # SpiNNaker imports
 from spinn_front_end_common.utilities.globals_variables import get_simulator
 from spinn_front_end_common.utilities.database.database_connection \
@@ -39,162 +41,23 @@ import spinn_gym as gym
 # -----------------------------------------------------------------------------
 vis_proc = None  # Visualiser process (global)
 
-
-# -----------------------------------------------------------------------------
-#  Helper Functions
-# -----------------------------------------------------------------------------
-def start_visualiser(database, pop_label, xr, yr, xb=8, yb=8, key_conn=None):
-    _, _, _, board_address, tag = database.get_live_output_details(
-        pop_label, "LiveSpikeReceiver")
-
-    print("Calling \'start_visualiser\'")
-
-    # Calling visualiser - must be done as process rather than on thread due to
-    # OS security (Mac)
-    global vis_proc
-    vis_proc = subprocess.Popen(
-        [sys.executable,
-         '../../spinn_gym/games/breakout/visualiser/visualiser.py',
-         board_address,
-         tag.__str__(),
-         xb.__str__(),
-         yb.__str__()
-         ])
-    # print("Visualiser proc ID: {}".format(vis_proc.pid))
-
-
-def get_scores(breakout_pop, simulator):
-    b_vertex = breakout_pop._vertex
-    scores = b_vertex.get_data(
-        'score', simulator.no_machine_time_steps, simulator.placements,
-        simulator.buffer_manager)
-
-    return scores.tolist()
-
-
-def row_col_to_input_breakout(row, col, is_on_input, row_bits, event_bits=1,
-                              colour_bits=2, row_start=0):
-    row_bits = np.uint32(row_bits)
-    idx = np.uint32(0)
-
-    if is_on_input:
-        idx = 1
-
-    row += row_start
-    idx = idx | (row << (colour_bits))  # colour bit
-    idx = idx | (col << (row_bits + colour_bits))
-
-    # add two to allow for special event bits
-    idx = idx + 2
-
-    return idx
-
-
-def subsample_connection(x_res, y_res, subsamp_factor_x, subsamp_factor_y,
-                         weight, coord_map_func):
-    # subY_BITS=int(np.ceil(np.log2(y_res/subsamp_factor)))
-    connection_list_on = []
-    connection_list_off = []
-
-    sx_res = int(x_res) // int(subsamp_factor_x)
-    row_bits = int(np.ceil(np.log2(y_res)))
-    for j in range(int(y_res)):
-        for i in range(int(x_res)):
-            si = i // subsamp_factor_x
-            sj = j // subsamp_factor_y
-
-            # ON channels
-            subsampidx = sj * sx_res + si
-            connection_list_on.append((coord_map_func(j, i, 1, row_bits),
-                                       subsampidx, weight, 1.))
-
-            # OFF channels only on segment borders
-            connection_list_off.append((coord_map_func(j, i, 0, row_bits),
-                                        subsampidx, weight, 1.))
-
-    return connection_list_on, connection_list_off
-
-
-# -----------------------------------------------------------------------------
-# Initialise Simulation and Parameters
-# -----------------------------------------------------------------------------
-
-# Game resolution
+# Game resolution and scaling
 X_RESOLUTION = 160
 Y_RESOLUTION = 128
+X_SCALE = 2
+Y_SCALE = 2
+LABEL = "Breakout"
 
-# UDP port to read spikes from
-UDP_PORT1 = 17886
-UDP_PORT2 = UDP_PORT1 + 1
+make_simulation(X_RESOLUTION, Y_RESOLUTION, X_SCALE, Y_SCALE, LABEL)
 
-# Setup pyNN simulation
-p.setup(timestep=1.0)
-p.set_number_of_neurons_per_core(p.IF_cond_exp, 128)
-
-# Game resolution sampling factors
-x_factor1 = 2
-y_factor1 = x_factor1
-bricking = 1
-
-# -----------------------------------------------------------------------------
-# Create Spiking Neural Network
-# -----------------------------------------------------------------------------
-
-# Create breakout population and activate live output
-b1 = gym.Breakout(x_factor=x_factor1, y_factor=y_factor1, bricking=bricking)
-breakout_pop = p.Population(b1.neurons(), b1, label="breakout1")
-
-# ex is the external device plugin manager
-ex.activate_live_output_for(breakout_pop)
-
-# Connect key spike injector to breakout population
-key_input = p.Population(2, SpikeInjector, label="key_input")
-key_input_connection = SpynnakerLiveSpikesConnection(send_labels=["key_input"], 
-                                                     local_port=None)
+key_input_connection = p.external_devices.SpynnakerLiveSpikesConnection(
+    send_labels=[key_input_label], local_port=None)
 p.external_devices.add_database_socket_address(
-     "localhost", key_input_connection.local_port, None)
-p.Projection(key_input, breakout_pop, p.AllToAllConnector(),
-             p.StaticSynapse(weight=0.1))
-
-# Create random spike input and connect to Breakout pop to stimulate paddle
-# (and enable paddle visualisation)
-spike_input = p.Population(2, p.SpikeSourcePoisson(rate=2),
-                           label="input_connect")
-p.Projection(spike_input, breakout_pop, p.AllToAllConnector(),
-             p.StaticSynapse(weight=0.1))
-
-weight = 0.1
-[Connections_on, Connections_off] = subsample_connection(
-    X_RESOLUTION/x_factor1, Y_RESOLUTION/y_factor1, 1, 1, weight,
-    row_col_to_input_breakout)
-
-# Create population of neurons to receive input from Breakout
-receive_pop_size = int(X_RESOLUTION/x_factor1) * int(Y_RESOLUTION/y_factor1)
-receive_pop = p.Population(receive_pop_size, p.IF_cond_exp(),
-                           label="receive_pop")
-p.Projection(breakout_pop, receive_pop, p.FromListConnector(Connections_on),
-             p.StaticSynapse(weight=weight))
-
-# Create population to receive reward signal from Breakout (n0: rew, n1: pun)
-receive_reward_pop = p.Population(2, p.IF_cond_exp(),
-                                  label="receive_rew_pop")
-p.Projection(breakout_pop, receive_reward_pop, p.OneToOneConnector(),
-             p.StaticSynapse(weight=0.1 * weight))
-
-# Setup recording
-spike_input.record('spikes')
-receive_pop.record('spikes')
-receive_reward_pop.record('all')
+    "localhost", key_input_connection.local_port, None)
 
 # -----------------------------------------------------------------------------
 # Configure Visualiser
 # -----------------------------------------------------------------------------
-print("UDP_PORT1: {}".format(UDP_PORT1))
-print("x_fact: {}, y_fact: {}".format(x_factor1, y_factor1))
-print("x_bits: {}, y_bits: {}".format(
-    np.uint32(np.ceil(np.log2(X_RESOLUTION/x_factor1))),
-    np.uint32(np.ceil(np.log2(Y_RESOLUTION/y_factor1)))
-    ))
 
 d_conn = DatabaseConnection(local_port=None)
 
