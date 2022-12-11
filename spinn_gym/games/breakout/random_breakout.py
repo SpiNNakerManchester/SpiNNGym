e52f5fa20f84fd12fa4754e9fa12a49e8efc75bb
Christian <christian.brenninkmeijer@manchester.ac.uk>
2022-12-11 23:11:13 +0100

    import pyNN.spiNNaker as sim

--- spinn_gym/games/breakout/random_breakout.py
+++ spinn_gym/games/breakout/random_breakout.py
@@ -12,7 +12,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-import spynnaker8 as p
+import pyNN.spiNNaker as sp
 from spinn_gym import Breakout
 from .breakout_sim import (
     subsample_connection, row_col_to_input_breakout)
