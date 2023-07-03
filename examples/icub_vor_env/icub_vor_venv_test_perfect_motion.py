b42f31071d1ed4f5fc0637494bcaa6547a10a7fd
Andrew Gait <andrew.gait@manchester.ac.uk>
2023-07-03 09:16:19 +0100

    Do imports more sensibly and update to single copyright year

--- examples/icub_vor_env/icub_vor_venv_test_perfect_motion.py
+++ examples/icub_vor_env/icub_vor_venv_test_perfect_motion.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019-2021 The University of Manchester
+# Copyright (c) 2021 The University of Manchester
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,15 +16,8 @@
 import pyNN.spiNNaker as p
 import spinn_gym as gym
 
-# from pyNN.utility.plotting import Figure, Panel
-# import matplotlib.pyplot as plt
 import numpy as np
-# import os
-from icub_utilities import (
-    generate_head_position_and_velocity, retrieve_and_package_results,
-    remap_odd_even, remap_second_half_descending, ICUB_VOR_VENV_POP_SIZE,
-    plot_results)
-
+import icub_utilities as icub_util
 
 # Parameter definition
 runtime = 3000
@@ -33,7 +26,7 @@ input_size = 200  # neurons
 output_size = 200  # neurons
 gain = 20.0
 
-head_pos, head_vel = generate_head_position_and_velocity(1)
+head_pos, head_vel = icub_util.generate_head_position_and_velocity(1)
 
 # perfect eye positions and velocities are exactly out of phase with head
 perfect_eye_pos = np.concatenate((head_pos[500:], head_pos[:500]))
@@ -80,7 +73,8 @@ output_pop = p.Population(output_size, p.SpikeSourcePoisson(rate=0))
 icub_vor_env_model = gym.ICubVorEnv(
     head_pos, head_vel, perfect_eye_vel, perfect_eye_pos, error_window_size,
     output_size)
-icub_vor_env_pop = p.Population(ICUB_VOR_VENV_POP_SIZE, icub_vor_env_model)
+icub_vor_env_pop = p.Population(
+    icub_util.ICUB_VOR_VENV_POP_SIZE, icub_vor_env_model)
 
 # Set recording for input and output pop (env pop records by default)
 input_pop.record('spikes')
@@ -98,7 +92,7 @@ p.external_devices.activate_live_output_to(
 p.run(runtime)
 
 # Get the data from the ICubVorEnv pop
-results = retrieve_and_package_results(icub_vor_env_pop)
+results = icub_util.retrieve_and_package_results(icub_vor_env_pop)
 
 # get the spike data from input and output
 spikes_in_spin = input_pop.spinnaker_get_data('spikes')
@@ -107,8 +101,9 @@ spikes_out_spin = output_pop.spinnaker_get_data('spikes')
 # end simulation
 p.end()
 
-remapped_vn_spikes = remap_odd_even(spikes_in_spin, input_size)
-remapped_cf_spikes = remap_second_half_descending(spikes_out_spin, output_size)
+remapped_vn_spikes = icub_util.remap_odd_even(spikes_in_spin, input_size)
+remapped_cf_spikes = icub_util.remap_second_half_descending(
+    spikes_out_spin, output_size)
 
 simulation_parameters = {
     'runtime': runtime,
@@ -123,7 +118,8 @@ simulation_parameters = {
 }
 
 # plot the data from the ICubVorEnv pop
-plot_results(results_dict=results, simulation_parameters=simulation_parameters,
-             name="spinngym_icub_vor_test_perfect")
+icub_util.plot_results(results_dict=results,
+                       simulation_parameters=simulation_parameters,
+                       name="spinngym_icub_vor_test_perfect")
 
 print("Done")
