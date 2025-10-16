fbf7bc1db3adfccc41175feb85486c72134e132f
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2025-10-16 17:49:41 +0100

    info for release 7.4.1

--- spinn_gym/_version.py
+++ spinn_gym/_version.py
@@ -13,8 +13,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-__version__ = "1!7.3.1"
-__version_month__ = "TBD"
-__version_year__ = "TBD"
-__version_day__ = "TBD"
-__version_name__ = "To Do"
+__version__ = "1!7.4.1"
+__version_month__ = "October"
+__version_year__ = "2025"
+__version_day__ = "16"
+__version_name__ = "Quietly Confident 1"
